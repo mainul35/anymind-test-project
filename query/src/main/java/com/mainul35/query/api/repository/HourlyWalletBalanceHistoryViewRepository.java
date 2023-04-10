74e563283ae656c35d704c19da8dc64fb3db582c
mainul35 <mainuls18@gmail.com>
2023-04-11 01:02:36 +0700

    ordering history by date ASC

--- query/src/main/java/com/mainul35/query/api/repository/HourlyWalletBalanceHistoryViewRepository.java
+++ query/src/main/java/com/mainul35/query/api/repository/HourlyWalletBalanceHistoryViewRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface HourlyWalletBalanceHistoryViewRepository extends JpaRepository<HourlyWalletBalanceHistoryView, Long> {
 
-    @Query("SELECT history FROM hourly_wallet_balance_history as history WHERE history.dateTime BETWEEN :begin AND :end AND history.wallerId = :walletId ")
+    @Query("SELECT history FROM hourly_wallet_balance_history as history WHERE history.dateTime BETWEEN :begin AND :end AND history.wallerId = :walletId ORDER BY history.dateTime ASC")
     List<HourlyWalletBalanceHistoryView> findBetweenDateTimesForWallet(@Param("begin") ZonedDateTime begin, @Param("end") ZonedDateTime end, @Param("walletId") String walletId);
 }
